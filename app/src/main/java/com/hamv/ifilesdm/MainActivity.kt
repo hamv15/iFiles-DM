536f32de3fadbf927a00156a85028db31f2fb311
Hugo Adrián Meza Vega <hamv15@hotmail.com>
2024-03-23 09:59:14 -0600

    Manejo de archivos con Json

--- app/src/main/java/com/hamv/ifilesdm/MainActivity.kt
+++ app/src/main/java/com/hamv/ifilesdm/MainActivity.kt
@@ -3,7 +3,9 @@ package com.hamv.ifilesdm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.gson.Gson
 import com.hamv.ifilesdm.databinding.ActivityMainBinding
+import com.hamv.ifilesdm.model.Student
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
@@ -22,8 +24,12 @@ class MainActivity : AppCompatActivity() {
     fun clickSave(view: View) {
         view.hideKeyboard()
         if (binding.tietText.text.toString().isNotEmpty()){
+
+            val student = Student(name = binding.tietText.text.toString())
+            val bytesToSave = Gson().toJson(student).encodeToByteArray()
+
             //Codificando a bytes la cadena de texto a almacenar
-            val bytesToSave = binding.tietText.text.toString().encodeToByteArray()
+            //val bytesToSave = binding.tietText.text.toString().encodeToByteArray()
             try {
                 val file = File(filesDir, "mi_archivo.txt")
                 if (!file.exists()){
@@ -64,9 +70,15 @@ class MainActivity : AppCompatActivity() {
                 val content = fis.readBytes()
                 binding.tvContent.text = content.decodeToString()
                 fis.close()*/
+                //binding.tvContent.text = file.readBytes().decodeToString()
 
-                binding.tvContent.text = file.readBytes().decodeToString()
+                //Para Json
+                val jsonString = file.readBytes().decodeToString()
+                val student = Gson().fromJson(jsonString, Student::class.java)
 
+                //binding.tvContent.text = "Id: ${student.id}, Nombre: ${student.name}"
+                //Forma correcta
+                binding.tvContent.text = getString(R.string.student, student.id.toString(), student.name)
             }else{
                 sbMessage(
                     binding.clRoot,
